87a5688f37116b57cb37102cd6d0d3b5bd79cbc0
Vishnu Vardhan JS <jaisrini76@gmail.com>
2024-07-16 00:45:52 +0530

    [Added] .env file

--- src/pages/accepted.js
+++ src/pages/accepted.js
@@ -1,38 +1,40 @@
-import React from 'react'
-import happy from '../assets/happy_cat.gif'
+import React from "react";
+import happy from "../assets/happy_cat.gif";
 
 function Accepted() {
-    const sendMessage = () => {
-        window.location.href = 'https://shorturl.at/iO8U3';
-      };
+  console.log(process.env.REACT_APP_WHATSAPP_LINK);
+  const sendMessage = () => {
+    window.location.href = process.env.REACT_APP_WHATSAPP_LINK;
+  };
 
-    function createHeart() {
-        const heart = document.createElement('div');
-        heart.classList.add('heart');
+  function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
 
-        heart.style.left = Math.random() * 100 + "vw";
-        heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
 
-        heart.innerText = '💗';
+    heart.innerText = "💗";
 
-        document.body.appendChild(heart);
+    document.body.appendChild(heart);
 
-        setTimeout(() => {
-            heart.remove();
-        }, 2000);
-    }
-    setInterval(createHeart, 1000);
-    return (
-        <div className="main-container">
-            <div className='items'>
-                <div className='container'>
-                    <img src={happy} className='cat-image' id='noButton' alt="rose_cat" />
-                    <div className='ask-out-text'>Happy Happy Happy!</div>
-                    <button onClick={sendMessage}>Send Message 💖</button>
-                </div>
-            </div>
+    setTimeout(() => {
+      heart.remove();
+    }, 2000);
+  }
+  setInterval(createHeart, 1000);
+  return (
+    <div className="main-container">
+      <div className="items">
+        <div className="container">
+          <img src={happy} className="cat-image" id="noButton" alt="rose_cat" />
+          <div className="ask-out-text">Happy Happy Happy!</div>
+          <button onClick={sendMessage}>Send Message 💖</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Accepted
+export default Accepted;
+
